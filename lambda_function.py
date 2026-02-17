053bb72395e10092face4ef1af97ac480035e5ce
Nicolas Ibanez <nicoibanez200@gmail.com>
2026-02-17 02:28:36 -0300

    feat: add read appointment function

--- lambda_function.py
+++ lambda_function.py
@@ -37,7 +37,28 @@ def save_appointment(data):
     response = table.put_item(Item=data)
     return response
 
+def get_appointment(appointment_id):
+    """
+    Retrives an appointment by ID from DunamoDB.
+
+    args:
+        appointment_id (str): unique appointment identifier
+
+    returns:
+        dict: appointment data or none if not found
+    """
+    response = table.get_item(Key={'appointment_id': appointment_id})
+    return response.get('Item', None)
+
 #Save the appointment
 result = save_appointment(appointment_data)
 print("Appointment saved successfully!")
 print(f"Appointment ID: {appointment_data['appointment_id']}")
+
+#Test reading the appointment we just created
+print("\n--- Testing READ ---")
+retrieved = get_appointment(appointment_data['appointment_id'])
+if retrieved:
+    print(f"Found appointment: patient: {retrieved['name_patient']} with Dr. {retrieved['name_doctor']}")
+else:
+    print("Appointment not found.")
